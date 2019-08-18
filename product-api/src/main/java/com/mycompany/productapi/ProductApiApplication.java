6f38869fa237183ef5d29c3739db87e303220bc9
Ivan Franchin <ivan.franchin@takeaway.com>
2019-08-18 16:47:50 +0200

    initial commit

---
+++ product-api/src/main/java/com/mycompany/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiApplication.class, args);
+    }
+
+}
---
+++ product-api/src/test/java/com/mycompany/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.productapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
